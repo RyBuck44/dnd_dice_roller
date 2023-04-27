4189416ab7fd3811d4c9b3b4ffdce816d520fe31
RyBuck44 <bucklerryan2@gmail.com>
2023-04-27 17:04:37 -0400

    established link with discord server and added working command

--- discord_dice_roller/bot.py
+++ discord_dice_roller/bot.py
@@ -1,2 +1,29 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import Droller
+
+token = 'MTEwMDQ4OTcwODc0NTgwNTg5Ng.GjLbIq.zMQ4uTzzFiaQIuzvjDa3FjzICfAwVENE7ICZik'
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+class db(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await tree.sync(guild=discord.Object(id=1100490695309017168))
+        self.synced = True
+        print('Bot is Online')
+
+
+bot = db()
+tree = app_commands.CommandTree(bot)
+
+
+@tree.command(name='dice_box', description='tells user what types of dice they can roll', guild=discord.Object(id=1100490695309017168))
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message('dice')
+
+bot.run(token)
