d74cf106201abcb5d8e1536ba5c63faf5233ce3d
RyBuck44 <bucklerryan2@gmail.com>
2023-04-28 00:57:36 -0400

    added dice roll command and function

--- discord_dice_roller/Droller.py
+++ discord_dice_roller/Droller.py
@@ -6,3 +6,13 @@ def coin_flip():
     result = random.choice(coin)
     return result
 
+
+def d_roller(die, rolls) -> int:
+    numbers = []
+    for number in range(rolls):
+        roll = random.randint(1, die)
+        numbers.append(roll)
+    g_total = list(numbers)
+    return g_total
+
+
--- discord_dice_roller/bot.py
+++ discord_dice_roller/bot.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
-from Droller import coin_flip
+from Droller import coin_flip, d_roller
 
 secret_code = os.getenv('DB_TOKEN')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -23,7 +23,7 @@ bot = db()
 tree = app_commands.CommandTree(bot)
 
 
-@tree.command(name='start', description='prompts the beginning of the rolling process', guild=discord.Object(id=1100490695309017168))
+@tree.command(name='help', description='prompts the beginning of the rolling process', guild=discord.Object(id=1100490695309017168))
 async def self(interaction: discord.Interaction):
     await interaction.response.send_message('Please enter all the dice you want me to roll. \n'
                                             'Currently I have D20s, D12s, D10s, D8s, D6s, D4s. \n'
@@ -38,6 +38,37 @@ async def self(interaction: discord.Interaction):
     outcome = coin_flip()
     await interaction.response.send_message(f"Ahh the old two sided die! \n"
                                             f"Here's what I got: {outcome}")
+
+
+@tree.command(name='roll', description='rolls selected dice', guild=discord.Object(id=1100490695309017168))
+async def self(interaction: discord.Interaction, dice:str):
+    available_dice = [
+        4,
+        6,
+        8,
+        10,
+        12,
+        20,
+    ]
+    all_dice = dice.split(',')
+    total_number = []
+    for dice in all_dice:
+        d = dice.split('d')
+        die = int(d[1])
+        rolls = int(d[0])
+        if die in available_dice:
+            total = d_roller(die, rolls)
+            total_number.append(total)
+        else:
+            break
+    all_roles = []
+    for sublist in total_number:
+        for item in sublist:
+            all_roles.append(item)
+    final_number = sum(all_roles)
+    await interaction.response.send_message(f"Dice rolled: {all_dice}\n"
+                                            f"Here's all your rolls: {all_roles}\n"
+                                            f"Here's the total: {final_number}")
 
 
 
